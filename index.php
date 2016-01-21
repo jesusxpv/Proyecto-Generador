02900fccebb3e6822795b2f73240526d932e6e7f
jesusxpv <jesusxpv@hotmail.com>
2016-01-21 10:05:04 -0600

    Segundo

--- index.php
+++ index.php
@@ -3,33 +3,177 @@
 <head>
 	<meta charset="utf-8"/>
 	<title>Generador</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
-<body>
+<body onload="start()">
 
+<div id="abc">
+	
+</div>
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">	
-		 	<form  class="text-center">  	
-				<ul class="list-inline">
-					<li><input type="text" class="form-control" placeholder="Nombre de columna"></li>
-					<li>
-						<select class="form-control">
-							<option value="">Nombre completo</option>
-							<option value="">Jesus</option>
-							<option value="">Jesus</option>
-						</select>
-					</li>
-					<li><input type="text" class="form-control" placeholder="Columna"></li>
-				</ul>
-				
-				<div class="form-group text-right">
-					<input type="text" class="btn-addfilas" placeholder="Añadir"/>
-					<button type="submit" class="btn btn-xl">fila(s)</button>	
+
+		<!--
+		<div class="col-md-4 col-md-offset-4">
+			<form class="text-center">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Número de filas" />
 				</div>
+				<div class="form-group">
+					<input type="button" value="Agregar" class="btn btn-xl" onclick="addFilas()" />
+				</div>
+			</form>
+		</div>
+		-->
+
+		<div class="col-md-8 col-md-offset-2">	
+		 	<form  id="nomFilas" class="text-center"> 
+		 		<div id="filasAgragadas">
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt1" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select  id="comb1" onchange="comb1Busqueda()" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option value="name">Nombre</option>
+		 						<option value="last">Apellido</option>
+		 						<option value="country">País</option>
+		 						<option value="state">Estado</option>
+		 						<option value="muni">Municipio</option>
+		 						<option value="cp">Codigo postal</option>
+		 						<option value="phone">Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt2" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb2" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt3" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb3" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt4" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb4" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt5" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb5" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt6" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb6" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt7" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb7" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt8" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select  id="comb8" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt9" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb9" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 			<ul class='list-inline'>
+		 				<li><input type='text' id="txt10" class='form-control' placeholder='Nombre de columna'></li>
+		 				<li><select id="comb10" class='form-control'>
+		 						<option>Seleccionar tipo de dato</option>
+		 						<option>Nombre</option>
+		 						<option>Apellido</option>
+		 						<option>País</option>
+		 						<option>Estado</option>
+		 						<option>Municipio</option>
+		 						<option>Codigo postal</option>
+		 						<option>Telefono</option>
+		 					</select>
+		 				</li>
+		 			</ul>
+		 		</div>			
   			</form>
 		</div>
 
@@ -58,6 +202,9 @@
 		  								<div class="form-group">
 		  							 		Caracter(es) delimitador(es) <input type="text" class="text-delim" placeholder="|">
 		  								</div>
+		  								<div class="form-group text-right">
+											<button type="button" class="btn btn-xl" onclick="btnGenerateCsv()" >Generar</button>
+										</div>
 		  							</form>
 		  						</div>
 		  					</div>
@@ -73,14 +220,11 @@
 											<input type="text" class="text-name-table" placeholder="Nombre de la tabla">
 										</div>
 		  								<div class="form-group">
-		  							 		<input type="CHECKBOX"> Incluir sentencia create table
-		  								</div>
-		  								<div class="form-group">
-		  							 		<input type="CHECKBOX"> Incluir sentencia drop table
-		  								</div>
-		  								<div class="form-group">
 		  							 		<input type="CHECKBOX"> Entrecomillar nombres (tablas-columnas)
 		  								</div>
+		  								<div class="form-group text-right">
+											<button type="button" class="btn btn-xl">Generar</button>
+										</div>
 		  							</form>
 		  						</div>
 		  					</div>
@@ -95,22 +239,9 @@
 
 
 
-
-		<div class="col-md-8 col-md-offset-2">
-			<form class="text-right">
-				<div class="form-group">
-					<button class="btn btn-xl">Generar</button>
-				</div>
-			</form>
-		</div><!-- end col -->
-	</div><!-- end row -->
-</div><!-- end container generar -->
-
-
-
-
 <script src="js/jquery-1.12.0.js"></script>
 <script src="js/bootstrap.js"></script>
+<script src="js/addFilas.js"></script>
 
 
 </body>
